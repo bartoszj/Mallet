ea6a0da1847c80569336858439827654babe536b
Bartosz Janda <bartosz.janda@gmail.com>
2013-11-05 21:37:20 +0100

    Added UIEdgeInsets and UIOffset. Added support for categories.

--- LLDBScripts/Summaries/UIKit.py
+++ LLDBScripts/Summaries/UIKit.py
@@ -24,20 +24,11 @@
 
 import lldb
 
-
-def SKProductsResponse_SummaryProvider(valobj, dict):
-    stream = lldb.SBStream()
-    valobj.GetExpressionPath(stream)
-    num_valid_products_o = valobj.CreateValueFromExpression("valid_count",
-                                        "(NSUInteger)[[" + stream.GetData() + " products] count]")
-    num_valid_products = num_valid_products_o.GetValueAsUnsigned()
-    num_not_valid_products_o = valobj.CreateValueFromExpression("not_valid_count",
-                                        "(NSUInteger)[[" + stream.GetData() + " invalidProductIdentifiers] count]")
-    num_not_valid_products = num_not_valid_products_o.GetValueAsUnsigned()
-
-    return "@\"Valid: {}, not valid: {}\"".format(num_valid_products, num_not_valid_products)
-
-
 def __lldb_init_module(debugger, dict):
-    debugger.HandleCommand("type summary add -F SKProductsResponse.SKProductsResponse_SummaryProvider --category StoreKit SKProductsResponse")
-    debugger.HandleCommand("type category enable StoreKit")
+    # UIEdgeInsets
+    debugger.HandleCommand("type summary add -s \"(top=${var.top}, left=${var.left}, bottom=${var.bottom}, right=${var.right})\" -C false -p -r -v --category UIKit UIEdgeInsets")
+
+    # UIOffset
+    debugger.HandleCommand("type summary add -s \"(horizontal=${var.horizontal}, vertical=${var.vertical})\" -C false -p -r -v --category UIKit UIOffset")
+
+    debugger.HandleCommand("type category enable UIKit")
