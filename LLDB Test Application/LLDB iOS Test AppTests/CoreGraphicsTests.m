52fc053d394619d94f529488d4fb4000a0e115b9
Bartosz Janda <bartosz.janda@gmail.com>
2015-01-20 20:43:10 +0100

    Added CGAffineTransform summary string.

--- LLDB Test Application/LLDB iOS Test AppTests/CoreGraphicsTests.m
+++ LLDB Test Application/LLDB iOS Test AppTests/CoreGraphicsTests.m
@@ -27,29 +27,39 @@
     [super tearDown];
 }
 
-#pragma mark - Tests
+#pragma mark - CGPoint
 - (void)testCGPoint
 {
     CGPoint point = CGPointMake(1, 2);
     [self compareVariable:&point ofType:@"CGPoint *" toSumamry:@"(x=1, y=2)"];
 }
 
+#pragma mark - CGSize
 - (void)testCGSize
 {
     CGSize size = CGSizeMake(3, 4);
     [self compareVariable:&size ofType:@"CGSize *" toSumamry:@"(width=3, height=4)"];
 }
 
+#pragma mark - CGRect
 - (void)testCGRect
 {
     CGRect rect = CGRectMake(6, 7, 8, 9);
     [self compareVariable:&rect ofType:@"CGRect *" toSumamry:@"origin=(x=6, y=7) size=(width=8, height=9)"];
 }
 
+#pragma mark - CGVector
 - (void)testCGVector
 {
     CGVector vector = CGVectorMake(3, 5);
     [self compareVariable:&vector ofType:@"CGVector *" toSumamry:@"(dx=3, dy=5)"];
+}
+
+#pragma mark - CGAffineTransform
+- (void)testCGAffineTransform
+{
+    CGAffineTransform transofrm = CGAffineTransformMake(2, 3, 4, 5, 6, 7);
+    [self compareVariable:&transofrm ofType:@"CGAffineTransform *" toSumamry:@"[2, 3], [4, 5], [6, 7]"];
 }
 
 @end
